f47aa6dec32431247c17282f71278ed9ad3162b5
Xadera <ragnarokha@gmail.com>
2017-07-01 12:01:29 +0300

    arrays problems

---
+++ Module 1/Arrays/Maximal K sum/MaximalKSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+public static class MaximalKSum
+{
+    public static void Main(string[] args)
+    {
+        var arrSize = int.Parse(Console.ReadLine());
+        var elementsToTake = int.Parse(Console.ReadLine());
+
+        var arr = new int[arrSize];
+        for (int i = 0; i < arrSize; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        var maxSum = arr
+            .OrderByDescending(x => x)
+            .Take(elementsToTake)
+            .Sum();
+        Console.WriteLine(maxSum);
+    }
+}
