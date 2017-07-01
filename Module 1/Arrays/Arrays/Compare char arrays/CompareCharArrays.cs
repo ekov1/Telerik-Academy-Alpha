96abdc6193c7602651880e46b98a9948e6023e12
Xadera <ragnarokha@gmail.com>
2017-07-01 09:49:09 +0300

    compare char arrays fix

--- Module 1/Arrays/Arrays/Compare char arrays/CompareCharArrays.cs
+++ Module 1/Arrays/Arrays/Compare char arrays/CompareCharArrays.cs
@@ -1,60 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 public static class CompareCharArrays
 {
     public static void Main(string[] args)
     {
-        var firstArray = Console.ReadLine();
-        var secondArray = Console.ReadLine();
+        var arrOne = Console.ReadLine();
+        var arrTwo = Console.ReadLine();
 
-        int maxLenght;
-        int smallerArray = 0;
-        if (firstArray.Length <= secondArray.Length)
+        if (arrOne == arrTwo)
         {
-            maxLenght = firstArray.Length;
+            Console.WriteLine('=');
         }
         else
         {
-            maxLenght = secondArray.Length;
-        }
+            var array = new [] {arrOne, arrTwo};
+            array = array.OrderBy(x => x).ThenBy(x => x.Length).ToArray();
 
-        for (int i = 0; i < maxLenght; i++)
-        {
-            if (firstArray[i] < secondArray[i])
+            if (array[0] == arrOne)
             {
-                smallerArray = 1;
-                break;
-            }
-            else if (firstArray[i] > secondArray[i])
-            {
-                smallerArray = 2;
-                break;
-            }
-        }
-
-        if (smallerArray == 1)
-        {
-            Console.WriteLine("<");
-        }
-        else if (smallerArray == 2)
-        {
-            Console.WriteLine(">");
-        }
-        else
-        {
-            if (firstArray.Length > secondArray.Length)
-            {
-                Console.WriteLine(">");
-            }
-            else if (firstArray.Length < secondArray.Length)
-            {
-                Console.WriteLine("<");
+                Console.WriteLine('<');
             }
             else
             {
-                Console.WriteLine("=");
+                Console.WriteLine('>');
             }
         }
     }
