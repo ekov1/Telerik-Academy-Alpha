aea746c9c0eae6dcb6b8619dd1341d6ea9628242
Xadera <ragnarokha@gmail.com>
2017-07-04 11:40:05 +0300

    ExtractSentences

--- Module 1/Strings/Extract sentences/ExtractSentences.cs
+++ Module 1/Strings/Extract sentences/ExtractSentences.cs
@@ -11,19 +11,18 @@ public static class ExtractSentences
         var sentence = Console.ReadLine();
 
         var matcher = new Regex(@"[A-Z].+?\.");
-        var matcherKeyword = new Regex(@"\W");
         var matches = matcher.Matches(sentence);
 
         var result = new StringBuilder();
         foreach (Match match in matches)
         {
-            if (matcherKeyword.Split(match.ToString()).ToArray().Contains(keyword))
+            if (match.ToString().ToLower().Split().ToArray().Contains(keyword))
             {
                 result.Append(match);
                 result.Append(' ');
             }
         }
 
-        Console.WriteLine(result.ToString().Trim());
+        Console.WriteLine(result.ToString().Trim(' '));
     }
 }
