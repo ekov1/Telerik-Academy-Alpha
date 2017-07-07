b1493b205bf81abbc12c576f70dc7b292d037f80
Xadera <ragnarokha@gmail.com>
2017-07-07 17:51:11 +0300

    all Exception Handling problems

---
+++ Module 1/Exception Handling/Enter numbers/EnterNumbers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class EnterNumbers
+{
+    public static void Main(string[] args)
+    {
+        var start = 1;
+        var end = 100;
+
+        var nunbers = new List<int>();
+        var exceptionNotThrown = true;
+        for (int i = 0; i < 10; i++)
+        {
+            var currentNumber = ReadNumber(start, end);
+
+            if (currentNumber > start && currentNumber < end)
+            {
+                nunbers.Add(currentNumber);
+                start = currentNumber;
+            }
+            else
+            {
+                exceptionNotThrown = false;      
+                break;
+            }
+        }
+
+        if (exceptionNotThrown)
+        {
+            Console.Write("1 < ");
+            Console.Write(string.Join(" < ", nunbers));
+            Console.Write(" < 100");
+        }
+        
+    }
+
+    public static int ReadNumber(int start, int end)
+    {
+        var number = 0;
+
+        try
+        {
+            number = int.Parse(Console.ReadLine());
+
+            if (number <= start || number >= end)
+            {
+                throw new Exception();
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Exception");
+        }
+
+        return number;
+    }
+}
---
+++ Module 1/Exception Handling/Square root/SquareRoot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class SquareRoot
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            var number = double.Parse(Console.ReadLine());
+
+            if (number < 0 || number > double.MaxValue)
+            {
+                throw new Exception();
+            }
+
+            Console.WriteLine("{0:f3}", Math.Sqrt(number));
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+}
