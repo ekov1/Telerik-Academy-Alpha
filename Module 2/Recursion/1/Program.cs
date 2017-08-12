8b0fb96f1007e3ba91d4960374f0330741284e8a
Xadera <ragnarokha@gmail.com>
2017-08-12 14:20:17 +0300

    recursion problem 1

---
+++ Module 2/Recursion/1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            PrintNestedLoops(5);
+        }
+
+        private static void PrintNestedLoops(int loopCount)
+        {
+            IterateNestedLoops(new Stack<int>(), 0, loopCount);
+        }
+
+        private static void IterateNestedLoops(Stack<int> nestedLoops, int currentIteration, int iterationCount)
+        {
+            if (currentIteration == iterationCount)
+            {
+                Console.WriteLine(string.Join(" ", nestedLoops.Reverse()));
+                return;
+            }
+
+            for (int currentNumber = 1; currentNumber <= iterationCount; currentNumber++)
+            {
+                nestedLoops.Push(currentNumber);
+                IterateNestedLoops(nestedLoops, currentIteration + 1, iterationCount);
+                nestedLoops.Pop();
+            }
+        }
+    }
+}
