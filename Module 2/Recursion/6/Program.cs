3912c6b155ce05321f709a0ed1e49dd9f8adf1c5
Xadera <ragnarokha@gmail.com>
2017-08-16 19:27:21 +0300

    Recursion 6

---
+++ Module 2/Recursion/6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var words = new string[] {"dog", "cat", "fish", "crocodile"};
+
+            PrintSubSetOfStrings(words, 0);
+        }
+
+        private static void PrintSubSetOfStrings(string[] words, int k)
+        {
+            if (k == words.Length - 1)
+            {
+                return;
+            }
+
+            for (int j = k; j < words.Length - 1; j++)
+            {
+                Console.WriteLine($"{words[k]} {words[j +  1]}");
+            }
+
+            PrintSubSetOfStrings(words, k + 1);
+        }
+    }
+}
