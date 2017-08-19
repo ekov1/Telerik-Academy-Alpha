3be6ce600d9e441df304ab9842401056ab956c24
Xadera <ragnarokha@gmail.com>
2017-08-19 20:52:49 +0300

    all problems for searching algorithms

---
+++ Module 2/Sorting and searching algorithms/BinarySearch/BinarySearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public static class BinarySearch
+    {
+        public static void Main(string[] args)
+        {
+            var arr = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine(arr.SearchBinary(0, 1, 9));
+        }
+
+        public static object SearchBinary(this IList<int> arr, int key, int min, int max)
+        {
+            if (min > max)
+            {
+
+                return "Nil";
+
+            }
+
+            int mid = (min + max) / 2;
+
+            if (key.Equals(arr[mid]))
+            {
+
+                return ++mid;
+
+            }
+
+            if (key < arr[mid])
+            {
+                return SearchBinary(arr, key, min, mid - 1);
+            }
+
+            return SearchBinary(arr, key, mid + 1, max);
+        }
+    }
+}
---
+++ Module 2/Sorting and searching algorithms/LinearSearch/LinearSearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace LinearSearch
+{
+    public static class LinearSearch
+    {
+        public static void Main(string[] args)
+        {
+            var arr = new[] {'a', 'b', 'c'};
+
+            Console.WriteLine(arr.SearchLinear('c'));
+        }
+
+        public static int SearchLinear(this IList arr,  object obj)
+        {         
+            for (var i = 0; i < arr.Count; i++)
+            {
+                if (arr[i].Equals(obj))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
