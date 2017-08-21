f1125db5424fa7215f6b3116da467153d55c2603
Xadera <ragnarokha@gmail.com>
2017-08-21 19:18:58 +0300

    Graphs readme and started friends of pesho

---
+++ Module 2/Graphs/Graphs/FriendsOfPesho/FriendsOfPesho.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendsOfPesho
+{
+    public class FriendsOfPesho
+    {
+        public static void Main(string[] args)
+        {
+            var inputLineOne = Console.ReadLine()
+                .Split(new [] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var numberOfDots = inputLineOne[0];
+            var numberOfStreets = inputLineOne[1];
+            var numberOfHospitals = inputLineOne[2];
+
+            var nodes = new Dictionary<int, char>();
+
+            var hospitals = Console.ReadLine()
+                .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var streets = new List<string>();
+            for (int i = 0; i < numberOfStreets; i++)
+            {
+                streets.Add(Console.ReadLine());
+            }
+        }
+    }
+}
