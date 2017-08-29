5442ab0b14f2b1027cc03b75dc04177b25a508bb
Xadera <ragnarokha@gmail.com>
2017-08-29 22:49:19 +0300

    Conference - viks solution

--- Module 2/DSA Practice/Conference/Conference.cs
+++ Module 2/DSA Practice/Conference/Conference.cs
@@ -8,28 +8,95 @@ namespace Conference
 {
     public class Conference
     {
+
         public static void Main(string[] args)
         {
-            var inputTokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Run();
+        }
 
-            var numberOfDevelopers = inputTokens[0];
-            var developerPairsNumber = inputTokens[1];
+        public static void Run()
+        {
+            int[] props = Console.ReadLine()
+                .Split().Select(int.Parse).ToArray();
 
-            var developerPairs = new List<int[]>();
-            for (int i = 0; i < developerPairsNumber; i++)
+            int numberOfPeople = props[0];
+            int numberOfPairs = props[1];
+
+            Dictionary<int, HashSet<int>> graph = new Dictionary<int, HashSet<int>>();
+            bool[] visited = new bool[numberOfPeople];
+
+            for (int i = 0; i < numberOfPairs; i++)
             {
-                developerPairs.Add(Console.ReadLine().Split().Select(int.Parse).ToArray());
+                int[] pair = Console.ReadLine()
+                    .Split().Select(int.Parse).ToArray();
+
+                int first = pair[0];
+                int second = pair[1];
+
+                if (!graph.ContainsKey(first))
+                {
+                    graph.Add(first, new HashSet<int>());
+                }
+
+                if (!graph.ContainsKey(second))
+                {
+                    graph.Add(second, new HashSet<int>());
+                }
+
+                graph[first].Add(second);
+                graph[second].Add(first);
             }
 
-
-            var result = 0;
-
-            for (int i = 1; i < developerPairs.Count; i++)
+            List<int> companiesPeopleCount = new List<int>();
+            foreach (int key in graph.Keys)
             {
-                result *= developerPairs[i].Length;
+                if (!visited[key])
+                {
+                    companiesPeopleCount.Add(DFS(key, graph, visited));
+                }
+            }
+
+            long result = 0;
+            long singles = numberOfPeople - graph.Keys.Count;
+
+            for (int i = 0; i < companiesPeopleCount.Count - 1; i++)
+            {
+                result += singles * companiesPeopleCount[i];
+                for (int j = i + 1; j < companiesPeopleCount.Count; j++)
+                {
+                    result += companiesPeopleCount[i]
+                              * companiesPeopleCount[j];
+                }
+            }
+
+            if (singles > 0)
+            {
+                result += singles * companiesPeopleCount[companiesPeopleCount.Count - 1];
+
+                result += (singles * (singles - 1)) / 2;
             }
 
             Console.WriteLine(result);
+        }
+
+        public static int DFS(int start,
+            Dictionary<int, HashSet<int>> graph, bool[] visited)
+        {
+            int result = 0;
+            if (visited[start])
+            {
+                return result;
+            }
+
+            result++;
+            visited[start] = true;
+            var successors = graph[start];
+            foreach (int succ in successors)
+            {
+                result += DFS(succ, graph, visited);
+            }
+
+            return result;
         }
     }
 }
