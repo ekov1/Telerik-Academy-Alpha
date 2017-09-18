5525e7a060c49fec77e2808140d1a09055bef59f
Xadera <ragnarokha@gmail.com>
2017-09-18 16:57:37 +0300

    academy DI + unit tests initial commit

---
+++ Module 2/HQC Practice/Academy/Before/Academy/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Academy.Core;
+using Academy.Ninject;
+using Ninject;
+
+namespace Academy
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            // Singleton design pattern
+            // Ensures that there is only one instance of Engine in existance
+            var kernel = new StandardKernel(new AcademyModule());
+            var engine = kernel.Get<Engine>();
+            engine.Start();
+        }
+    }
+}
