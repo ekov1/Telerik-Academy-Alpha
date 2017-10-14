5b3dcf1550a62a9598891e136e1295de72f86337
Xadera <ragnarokha@gmail.com>
2017-10-14 16:18:12 +0300

    entityframework demo

---
+++ Module 3/EntityFramework/EntityFrameworkDemo/Demo/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var db = new TelerikAcademyEntities();
+            db.Towns.Add(
+                new Town
+                {
+                    Name = "Melnik"
+                });
+
+            db.SaveChanges();
+        }
+    }
+}
